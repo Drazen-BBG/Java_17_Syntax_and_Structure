d30b1ba8dbd803cc02d2013071fc8312cddaf568
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-04 23:05:45 +0200

    challange1 fill in the blanks start

---
+++ src/challange1_fill_in_the_blanks/FillInTheBlanks.java
@@ -0,0 +1,21 @@
+package challange1_fill_in_the_blanks;
+
+public class FillInTheBlanks {
+
+    public static void main(String[] args) {
+        // Challenge: Create a fill-in-the-blanks story program
+        //On a ADJECTIVE SEASON day, I drink a minimum of NUMBER cups of coffee
+
+        //Program requirements
+
+        //Ask the user to provide challenge
+        //1. Season of the year
+        //2. Whole number (Integer)
+        //3. Adjective
+
+        //4. Store the inputs in variables
+
+        //5. Print a story by filling in the blanks with variables
+
+    }
+}
