4fc08fec8401d38358633b8a8cc43136832d07c3
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-04 22:21:12 +0200

    gross calculator TODO

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,4 +1,16 @@
 package gross_calculator;
 
 public class GrossPayCalculator {
+    public static void main(String[] args) {
+        //Steps to implement gross calculator algorithms
+
+        //1.Get the number of hours worked
+
+        //2.Get the hourly pay rate
+
+        //3.Multiply hours and pay rate
+
+        //4.Display result
+
+    }
 }
