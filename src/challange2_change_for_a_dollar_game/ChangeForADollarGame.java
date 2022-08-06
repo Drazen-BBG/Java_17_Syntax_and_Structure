5d1cd46e9692b642431302da544bfec3415bbaae
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-06 15:36:05 -0400

    challange2 Change for a dollar game START

---
+++ src/challange2_change_for_a_dollar_game/ChangeForADollarGame.java
@@ -0,0 +1,27 @@
+package challange2_change_for_a_dollar_game;
+
+    //Challenge2 Change for a dollar game!
+    //This is what I know
+    //A penny is worth 1 cent. A nickel is worth 5 cents. A dime is worth 10 cents. A quarter is worth 25 cents.
+
+    // Ask the user to provide
+    //Number of pennies
+    //Number of nickels
+    //Number of dimes
+    //Number of quarters
+
+    //Calculate and print
+    //Count total value of the coins
+    //Print message informing them if they won or lost - IF the change is equal $1 they win otherwise they lose the game
+    //If they lost, let them know how much they were over or under $1
+
+public class ChangeForADollarGame {
+
+    //1.Ask the user how many of each of those they want
+
+    //2.Program needs to know what is the worth of each of those coins
+
+    //3.Do the math
+
+    //4.Inform the player
+}
