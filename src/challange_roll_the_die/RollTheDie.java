81ea372f89b7312b658e40bf63e1eb85bf867a99
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-06 18:56:04 -0400

    challange3 roll the die START

---
+++ src/challange_roll_the_die/RollTheDie.java
@@ -0,0 +1,22 @@
+package challange_roll_the_die;
+
+    //Challenge Roll the Die
+    //Travel the entire game board of 20 spaces withing 5 die rolls
+    //Random random = new Random();
+    //int die = random.nextInt(6) + 1;
+
+    //Program requirements
+    //Generate random number between 1 and 6 to represent die roll
+    //Tell the player which game space they are on and how many more spaces they have to go to win
+    //Repeat four times (five rolls total)
+    //If the player advances exactly 20 spaces withing five rolls, they win
+
+    //Output the result of the game
+
+    // Roll #1: You've rolled a 3. You are now on space 3 and have 17 more to go
+
+
+public class RollTheDie {
+
+
+}
