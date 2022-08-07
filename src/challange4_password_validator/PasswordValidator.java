e6cd2fe0f1702651c01f98a9fee9b530c97ea7a7
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-07 14:06:25 -0400

    challenge4 Password Validator START

---
+++ src/challange4_password_validator/PasswordValidator.java
@@ -0,0 +1,29 @@
+package challange4_password_validator;
+
+    //Challenge Password Validator
+    //Validate the complexity of a proposed password
+
+    //Program Requirements
+
+    //Given
+    //Username: johndoe
+    //Password: ABC_1234
+
+    //Ask the user to provide
+    //New password
+
+    //Verify
+    //At least 8 characters long
+    //Contains an uppercase letter
+    //Contains a special character
+    //Does not contain the username (johndoe)
+    //is not same as the old password (ABC_1234)
+
+public class PasswordValidator {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
