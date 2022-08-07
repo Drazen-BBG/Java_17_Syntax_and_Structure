2cc59a70151fbd0ff1dc1f47daf1d490095d72b9
Drazen Drinic <drazendrinic@hotmail.com>
2022-08-07 07:09:21 -0400

    Returning data from methods

--- src/methods/InstantCreditCheck.java
+++ src/methods/InstantCreditCheck.java
@@ -22,18 +22,31 @@ public class InstantCreditCheck {
         scanner.close();
 
         //Check if the user is qualified
-        isUserQualified(creditScore, salary);
+        boolean qualified = isUserQualified(creditScore, salary);
+
+        notifyUser(qualified);
 
     }
 
-    public static void isUserQualified(int creditScore, double salary){
+    public static boolean isUserQualified(int creditScore, double salary){
         double requiredSalary = 25000;
         int requiredCreditScore = 700;
 
         if (creditScore>=requiredCreditScore && salary >= requiredSalary){
-            System.out.println("Congrats! You've been approved!");
+            //System.out.println("Congrats! You've been approved!");
+            return true;
         }else {
-            System.out.println("Sorry, you've been declined.");
+            //System.out.println("Sorry, you've been declined.");
+            return false;
+        }
+
+    }
+
+    public static void notifyUser(boolean isQualified){
+        if (isQualified){
+            System.out.println("Congrats! You have been approved");
+        }else {
+            System.out.println("Sorry. You've been declined.");
         }
     }
 }
